190a689b6099ddc34d5ff9bbc317ebc5d2159389
is2mael <ismael.muh26@gmail.com>
2024-10-08 19:30:13 +0700

    Feat: add Navbar

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Navbar from "./Components/Navbar";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Homepage />,
+    element: 
+    <>
+    <Navbar/>
+    <Homepage />
+    </>
   },
 ]);
 
